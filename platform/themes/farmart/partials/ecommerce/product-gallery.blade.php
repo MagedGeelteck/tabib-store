991a03491e24d7949cb0e256d715e8662398121a
MagedGeelteck <dev54@geeltech.com>
2025-10-18 16:09:04 +0300

    Image paths

--- platform/themes/farmart/partials/ecommerce/product-gallery.blade.php
+++ platform/themes/farmart/partials/ecommerce/product-gallery.blade.php
@@ -1,11 +1,48 @@
 <div class="product-gallery product-gallery--with-images row">
     <div class="product-gallery__wrapper">
+        @php
+            $identifier = $product->sku ?: $product->id;
+        @endphp
         @forelse ($productImages as $key=> $img)
+            @php
+                $originalUrl = RvMedia::getImageUrl($img, null, false, RvMedia::getDefaultImage());
+                $filename = basename(parse_url($originalUrl, PHP_URL_PATH));
+                $compressedRelative = "storage/compressed-images/products-images/{$identifier}/{$filename}";
+                $compressedPath = public_path($compressedRelative);
+
+                $useCompressed = false;
+                $compressedUrl = '';
+                $width = $height = null;
+                if (file_exists($compressedPath)) {
+                    $useCompressed = true;
+                    $compressedUrl = asset($compressedRelative);
+                    try {
+                        $size = @getimagesize($compressedPath);
+                        if (!empty($size)) {
+                            $width = $size[0];
+                            $height = $size[1];
+                        }
+                    } catch (\Exception $e) {
+                        // ignore
+                    }
+                }
+
+                $imgUrl = $useCompressed ? $compressedUrl : RvMedia::getImageUrl($img);
+                $thumbUrl = $useCompressed ? $compressedUrl : RvMedia::getImageUrl($img, 'thumb');
+            @endphp
             <div class="product-gallery__imag item">
                 <a class="img-fluid-eq" data-bs-toggle="modal" data-bs-target="#exampleModal{{$key}}">
                     <div class="img-fluid-eq__dummy"></div>
                     <div class="img-fluid-eq__wrap">
-                        <img class="mx-auto" title="{{ $product->name }}" src="{{ image_placeholder($img) }}" data-lazy="{{ RvMedia::getImageUrl($img) }}">
+                        <img
+                            class="mx-auto lazyload"
+                            title="{{ $product->name }}"
+                            src="{{ image_placeholder($img) }}"
+                            data-src="{{ $imgUrl }}"
+                            @if($width && $height) width="{{ $width }}" height="{{ $height }}" @endif
+                            @if ($loop->first) loading="eager" fetchpriority="high" @else loading="lazy" @endif
+                            decoding="async"
+                        >
                     </div>
                 </a>
             </div>
@@ -15,9 +52,19 @@
     </div>
     <div class="product-gallery__variants px-1 py-1">
         @forelse ($productImages as $img)
+            @php
+                $originalThumb = RvMedia::getImageUrl($img, null, false, RvMedia::getDefaultImage());
+                $thumbFilename = basename(parse_url($originalThumb, PHP_URL_PATH));
+                $compressedThumbRelative = "storage/compressed-images/products-images/{$identifier}/{$thumbFilename}";
+                $compressedThumbPath = public_path($compressedThumbRelative);
+                $t = RvMedia::getImageUrl($img, 'thumb');
+                if (file_exists($compressedThumbPath)) {
+                    $t = asset($compressedThumbRelative);
+                }
+            @endphp
             <div class="item">
                 <div class="border p-1 m-1">
-                    <img class="lazyload" title="{{ $product->name }}" src="{{ image_placeholder($img, 'thumb') }}" data-src="{{ RvMedia::getImageUrl($img, 'thumb') }}">
+                    <img class="lazyload" title="{{ $product->name }}" src="{{ image_placeholder($t) }}" data-src="{{ $t }}">
                 </div>
             </div>
         @empty
@@ -27,6 +74,13 @@
 </div>
 
 @forelse ($productImages as $key=> $img)
+@php
+    $originalUrl = RvMedia::getImageUrl($img, null, false, RvMedia::getDefaultImage());
+    $filename = basename(parse_url($originalUrl, PHP_URL_PATH));
+    $compressedRelative = "storage/compressed-images/products-images/{$identifier}/{$filename}";
+    $compressedPath = public_path($compressedRelative);
+    $modalSrc = file_exists($compressedPath) ? asset($compressedRelative) : RvMedia::getImageUrl($img, 'thumb');
+@endphp
 <div class="modal fade" id="exampleModal{{$key}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered modal-lg photo-galery-with-zoom">
     <div class="modal-content"> 
@@ -34,7 +88,7 @@
         <button type="button" class="btn btn-dark"  data-bs-dismiss="modal" aria-label="Close" style="z-index:100; margin-top:-400px!important; font-size:16px; border:1px solid red;">Close <i class="fa fa-window-close-o" aria-hidden="true"></i></button>
       </div>
       <div class="modal-body gallery-modal-body">
-     <img  class="lazyload" title="{{ $product->name }}" src="{{ image_placeholder($img, 'thumb') }}" data-src="{{ RvMedia::getImageUrl($img, 'thumb') }}">
+     <img  class="lazyload" title="{{ $product->name }}" src="{{ image_placeholder($modalSrc, 'thumb') }}" data-src="{{ $modalSrc }}">
 
       </div>
     </div>
